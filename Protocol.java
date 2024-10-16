f4b489681f241dbb170b48d78f8ce51cfb4d6fcd
joelgriffin53 <c3018412@newcastle.ac.uk>
2024-10-16 10:11:03 +0100

    create the packet

--- Protocol.java
+++ Protocol.java
@@ -7,10 +7,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.net.*;
 
 public class Protocol {
 
@@ -85,6 +82,20 @@ public class Protocol {
 			ByteArrayOutputStream byteAOutputStream = new ByteArrayOutputStream();
 			ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteAOutputStream);
 			objectOutputStream.writeObject(metaData);
+			objectOutputStream.flush();
+			byte[] metaDataBytes = byteAOutputStream.toByteArray();
+
+			DatagramPacket metaPacket = new DatagramPacket(
+					metaDataBytes,
+					metaDataBytes.length,
+					ipAddress,
+					portNumber
+			);
+
+
+
+
+
 
 
 
